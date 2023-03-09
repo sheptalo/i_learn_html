3a727b8ce4e1b073d6a87ded70c2f787a0815e4a
sheptalo <solovovartem99@gmail.com>
2023-03-09 16:49:12 +0300

    changed painter

--- another_pages/proggrams/painter/painter.js
+++ another_pages/proggrams/painter/painter.js
@@ -1,13 +1,23 @@
-const color = document.querySelector('#color');
-const get_color = document.getElementById("get_color");
-const block = document.querySelector('.point')    
-color.addEventListener("input", function(){
-    get_color.innerHTML = color.value;  
+
+let get_color = document.getElementById("get_color");
+const block = document.querySelector(".point");
+const fill = document.querySelector("#fill");
+
+document.querySelectorAll('.color').forEach(function(i){
+    i.addEventListener("click", function(){
+        get_color.setAttribute("value", i.id);
+      });
 });
-     document.querySelectorAll('.point').forEach(function(i) {
-        i.addEventListener("click", function(){
-            i.style.backgroundColor = color.value;
-        });
-    
-    });;
+
+document.querySelectorAll(".point").forEach(function (i) {
+  i.addEventListener("click", function () {
+    i.style.backgroundColor = get_color.value;
+  });
+});
+fill.addEventListener("click", function () {
+  console.log("nice");
+  document.querySelectorAll(".point").forEach(function (i) {
+    i.style.backgroundColor = get_color.value;
+  });
+});
 
