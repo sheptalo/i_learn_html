8b13e80b7ca86184d2c327d85d88847e29e2ecee
sheptalo <solovovartem99@gmail.com>
2023-03-10 21:29:30 +0300

    mb last update for painter

--- another_pages/proggrams/painter/painter.js
+++ another_pages/proggrams/painter/painter.js
@@ -2,18 +2,27 @@
 let get_color = document.getElementById("get_color");
 const block = document.querySelector(".point");
 const fill = document.querySelector("#fill");
+let createColorText = document.querySelector("#createColorText");
+const createColor = document.querySelector("#createColor");
+function setcolor(){
+  document.querySelectorAll(".color").
+  forEach(function(i){
+    i.style.backgroundColor = i.id;
+      i.addEventListener("click", function(){
+          get_color.setAttribute("value", i.id);
+        });
+  });
+}
 
-document.querySelectorAll('.color').forEach(function(i){
-    i.addEventListener("click", function(){
-        get_color.setAttribute("value", i.id);
-      });
-});
+setcolor();
 
-document.querySelectorAll(".point").forEach(function (i) {
+document.querySelectorAll(".point").
+forEach(function (i) {
   i.addEventListener("click", function () {
     i.style.backgroundColor = get_color.value;
   });
 });
+
 fill.addEventListener("click", function () {
   console.log("nice");
   document.querySelectorAll(".point").forEach(function (i) {
@@ -21,3 +30,8 @@ fill.addEventListener("click", function () {
   });
 });
 
+createColor.addEventListener("click", function(){
+  console.log("sucess")
+  document.querySelector("#createCol").setAttribute("id", createColorText.value);
+  setcolor();
+})
