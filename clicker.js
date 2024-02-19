f0a47a1d4185bc0b608fbfce521e10bad2013396
sheptalo <solovovartem99@gmail.com>
2024-02-20 00:21:21 +0300

    update

--- clicker.js
+++ clicker.js
@@ -1,14 +1,16 @@
-let money = 0
+let money = 0;
+let tg = window.Telegram.WebApp;
+let work = document.getElementById('work');
+let finish = document.getElementById('finish');
 
-let work = document.getElementById('work')
-let finish = document.getElementById('finish')
-
+tg.expand();
 work.addEventListener('click', function func() {
-  money += 1
-  console.log(money)
+  money += 1;
+  console.log(money);
 })
 
 finish.addEventListener('click', function func() {
   // отправить данные в тг
-  money = 0
+  tg.sendData(money);
+  money = 0;
 })
