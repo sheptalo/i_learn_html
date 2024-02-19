62bd8ed76c9a84e36e606c947314bf1e75d44f57
sheptalo <solovovartem99@gmail.com>
2024-02-20 00:37:12 +0300

    update clicker.js

--- clicker.js
+++ clicker.js
@@ -3,13 +3,19 @@ let tg = window.Telegram.WebApp;
 let work = document.getElementById('work');
 tg.MainButton.show()
 
+tg.expand(); //расширяем на все окно  
+tg.MainButton.text = "Закончить работу";
+tg.MainButton.textColor = "#ffffff"; //изменяем цвет текста кнопки
+tg.MainButton.color = "#333333"; //изменяем цвет бэкграунда кнопки
+tg.MainButton.setParams({"color": "#143F6B"});
+
 tg.expand();
 work.addEventListener('click', function func() {
   money += 1;
   console.log(money);
 })
 
-tg.MainButton.text = "Закончить работу";
+
 
 Telegram.WebApp.onEvent('mainButtonClicked', function(){
 	tg.sendData(money); 
