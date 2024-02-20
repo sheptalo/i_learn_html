60afe9f6d5f80311493e60cc9ec6b43ca9f25f94
sheptik <solovovartem99@gmail.com>
2024-02-20 21:34:20 +0300

    sdfsdf

--- clicker.js
+++ clicker.js
@@ -1,7 +1,9 @@
 let money = 0;
+let bot_unlocked = false
+let lvl = 0;
 let tg = window.Telegram.WebApp;
 let work = document.getElementById('work');
-
+let buy_bot = document.getElementById('bot')
 
 tg.expand(); //расширяем на все окно  
 tg.MainButton.text = "Закончить работу";
@@ -19,6 +21,21 @@ Telegram.WebApp.onEvent('mainButtonClicked', function(){
 });
 
 
+setInterval(() => {
+  if (bot_unlocked){
+  money += 1 * lvl;
+  updateMoneyText()
+  }
+}, 1000);
+buy_bot.addEventListener('click', function(){
+  if (money >= (lvl+1)*100){
+    money-=(lvl+1)*100;
+    lvl+=1;
+    bot_unlocked = true;
+    buy_bot.innerHTML = 'улучшить-авто ' + (lvl+1)*100
+    updateMoneyText()
+  }
+})
 work.addEventListener('click', function func() {
   money += 1;
   updateMoneyText();
